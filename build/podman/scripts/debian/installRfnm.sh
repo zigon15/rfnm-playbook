68c1fc035c1bda9e82231f758aac1b8c2473cc2d
Simon <simonzigon15@gmail.com>
2026-02-08 10:25:14 +1030

    Add built from source la9310 freertos

--- build/podman/scripts/debian/installRfnm.sh
+++ build/podman/scripts/debian/installRfnm.sh
@@ -28,17 +28,15 @@ depmod -b "$ABS_OUTPUT_DIR" "$KERNEL_VERSION"
 echo "Done! Modules installed to: $MODULE_DIR"
 
 # #---- Install LA9310 Firmware ----#
-# FIRMWARE_DIR="$ABS_OUTPUT_DIR/lib/firmware"
-# mkdir -p "$FIRMWARE_DIR"
+FIRMWARE_DIR="$ABS_OUTPUT_DIR/lib/firmware"
+mkdir -p "$FIRMWARE_DIR"
 
-# echo "Installing LA9310 firmware to: $FIRMWARE_DIR"
+echo "Installing LA9310 firmware to: $FIRMWARE_DIR"
 
-# # Copy the LA9310 FreeRTOS firmware binary
-# cp -v /work/build/la9310-freertos/Demo/CORTEX_M4_NXP_LA9310_GCC/release/la9310.bin "$FIRMWARE_DIR/la9310.bin"
-# cp -v /work/build/la9310-freertos/standalone_utils/firmware/apm.eld "$FIRMWARE_DIR/apm.eld"
+# Copy the LA9310 FreeRTOS firmware binary
+cp -v /work/build/la9310-freertos/Demo/CORTEX_M4_NXP_LA9310_GCC/release/la9310.bin "$FIRMWARE_DIR/la9310.bin"
 
-# # Set permissions
-# chmod 644 "$FIRMWARE_DIR/la9310.bin"
-# chmod 644 "$FIRMWARE_DIR/apm.eld"
+# Set permissions
+chmod 644 "$FIRMWARE_DIR/la9310.bin"
 
-# echo "Done! Firmware installed to: $FIRMWARE_DIR/la9310.bin and $FIRMWARE_DIR/apm.eld"
+echo "Done! Firmware installed to: $FIRMWARE_DIR/la9310.bin"
